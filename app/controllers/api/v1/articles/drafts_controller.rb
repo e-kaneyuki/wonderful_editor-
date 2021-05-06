3f7e050a1e991e6a4d16fb5bc720bcd328487af1
e-kaneyuki <occultchikin@gmail.com>
2021-05-06 22:23:02 +0900

    draftのindexメソッドでのActiveRecordを修正ver.2

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Articles::DraftsController < ApplicationController
   # このControllerは下書きをindexで一覧表示できるか、showで詳細を選択的に表示するかを決めるものとして設定していく
   def index
     # binding.pry
-    articles = Article.where(status: 0)
+    @articles = Article.where(status: 0)
     render json: @articles, each_serializer: ArticlePreviewSerializer
   end
 
